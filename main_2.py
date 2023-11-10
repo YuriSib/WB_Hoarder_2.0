a8892ba10b82a32f749908dc256681a0c1573da9
YuriSib <sib_94@vk.com>
2023-11-10 19:55:22 +0300

    Начал переписывать алгоритм, чтобы он сравнивал цену со средней ценой на Авито, и выводил сообщение в случае N-ной разницы. Сделал вывод сообщения в телеграм-канал.

--- main_2.py
+++ main_2.py
@@ -1,12 +1,10 @@
 import time
 
 from main import compare
-from avito_price import get_price
 from wb_master import get_category, get_product
 from sql_master import check_id, save_price_wb_table, load_row_for_id, save_in_wb_table, save_in_search_table,\
     save_average_price
-from tg_master import avito_message
-from pattern_extracting import get_model
+from tg_master import avito_message, error_message
 from url_master import category_url
 
 
@@ -14,35 +12,39 @@ def main(url, category):
     category_list = get_category(url)
 
     for product in category_list:
-        name, brand = product['Наименование'], product['Бренд'],
-        price, id_ = product['Цена со скидкой'], product['Артикул, id']
+        try:
+            name, brand = product['Наименование'], product['Бренд'],
+            price, id_ = product['Цена со скидкой'], product['Артикул, id']
 
-        if 'akita' in brand or 'AKITA' in brand or 'osch' in brand or 'OSCH' in brand or 'ewalt' in brand or \
-                'EWALT' in brand or 'eWalt' in brand or 'eWalt' in brand:
-            continue
-
-        if check_id(id_, 'wb_table'):
-            wb_name, average_price = load_row_for_id(id_, 'wb_table')[1], load_row_for_id(id_, 'wb_table')[4]
-            photo_link = load_row_for_id(id_, 'wb_table')[2]
-            save_price_wb_table(price, id_)
-
-            if average_price == int or average_price == float:
-                check_difference = compare(price, average_price, 15)
-                if check_difference:
-                    avito_message(wb_name, id_, price, average_price, photo_link)
-
-        else:
-            dirty_name, description, photo_link, average_price = get_product(id_, category)
-            save_in_wb_table(id_, description, photo_link, price, average_price)
-
-            if average_price == int or average_price == float:
-                check_difference = compare(price, average_price, 15)
-                save_average_price(id_, average_price)
-
-                if check_difference:
-                    avito_message(description, id_, price, average_price, photo_link)
-            else:
+            if 'akita' in brand or 'AKITA' in brand or 'osch' in brand or 'OSCH' in brand or 'ewalt' in brand or \
+                    'EWALT' in brand or 'eWalt' in brand or 'eWalt' in brand:
                 continue
+
+            if check_id(id_, 'wb_table'):
+                wb_name, average_price = load_row_for_id(id_, 'wb_table')[1], load_row_for_id(id_, 'wb_table')[4]
+                photo_link = load_row_for_id(id_, 'wb_table')[2]
+                save_price_wb_table(price, id_)
+
+                if average_price == int or average_price == float:
+                    check_difference = compare(price, average_price, 15)
+                    if check_difference:
+                        avito_message(wb_name, id_, price, average_price, photo_link)
+
+            else:
+                dirty_name, description, photo_link, average_price = get_product(id_, category)
+                save_in_wb_table(id_, description, photo_link, price, average_price)
+
+                if average_price == int or average_price == float:
+                    check_difference = compare(price, average_price, 15)
+                    save_average_price(id_, average_price)
+
+                    if check_difference:
+                        avito_message(description, id_, price, average_price, photo_link)
+                else:
+                    continue
+        except Exception as e:
+            error_message(e)
+            continue
 
 
 category_dict = category_url()
